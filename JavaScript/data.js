07a060a953b7e16d63c6a4ed0a5b8b883b80b870
EngineerJuliánEscobar <109046907+julianescobarco@users.noreply.github.com>
2023-04-03 19:58:44 -0500

    Creación Pagina 1

---
+++ JavaScript/data.js
@@ -0,0 +1,136 @@
+  const Relojes = [
+    {
+        bar_id: 1,
+        imagen: "https://amazingeventsapi.herokuapp.com/api/img/Cine7.jpg",
+        descripcion: "Hola",
+        Marca: "Apple",
+        Tipo: "Analogo, Band, Watch",
+        quantity: 10,
+        price: 5.99
+    },
+    {
+        bar_id: 1,
+        imagen: "https://amazingeventsapi.herokuapp.com/api/img/Cine7.jpg",
+        descripcion: "Hola",
+        Marca: "Apple",
+        Tipo: "Analogo, Band, Watch",
+        quantity: 10,
+        price: 5.99
+    },
+    {
+        bar_id: 1,
+        imagen: "https://amazingeventsapi.herokuapp.com/api/img/Cine7.jpg",
+        descripcion: "Hola",
+        Marca: "Apple",
+        Tipo: "Analogo, Band, Watch",
+        quantity: 10,
+        price: 5.99
+    },
+    {
+        bar_id: 1,
+        imagen: "https://amazingeventsapi.herokuapp.com/api/img/Cine7.jpg",
+        descripcion: "Hola",
+        Marca: "Apple",
+        Tipo: "Analogo, Band, Watch",
+        quantity: 10,
+        price: 5.99
+    },
+    {
+        bar_id: 1,
+        imagen: "https://amazingeventsapi.herokuapp.com/api/img/Cine7.jpg",
+        descripcion: "Hola",
+        Marca: "Apple",
+        Tipo: "Analogo, Band, Watch",
+        quantity: 10,
+        price: 5.99
+    },
+    {
+        bar_id: 1,
+        imagen: "https://amazingeventsapi.herokuapp.com/api/img/Cine7.jpg",
+        descripcion: "Hola",
+        Marca: "Apple",
+        Tipo: "Analogo, Band, Watch",
+        quantity: 10,
+        price: 5.99
+    },
+    {
+        bar_id: 1,
+        imagen: "https://amazingeventsapi.herokuapp.com/api/img/Cine7.jpg",
+        descripcion: "Hola",
+        Marca: "Apple",
+        Tipo: "Analogo, Band, Watch",
+        quantity: 10,
+        price: 5.99
+    },
+    {
+        bar_id: 1,
+        imagen: "https://amazingeventsapi.herokuapp.com/api/img/Cine7.jpg",
+        descripcion: "Hola",
+        Marca: "Apple",
+        Tipo: "Analogo, Band, Watch",
+        quantity: 10,
+        price: 5.99
+    },
+    {
+        bar_id: 1,
+        imagen: "https://amazingeventsapi.herokuapp.com/api/img/Cine7.jpg",
+        descripcion: "Hola",
+        Marca: "Apple",
+        Tipo: "Analogo, Band, Watch",
+        quantity: 10,
+        price: 5.99
+    },
+    {
+        bar_id: 1,
+        imagen: "https://amazingeventsapi.herokuapp.com/api/img/Cine7.jpg",
+        descripcion: "Hola",
+        Marca: "Apple",
+        Tipo: "Analogo, Band, Watch",
+        quantity: 10,
+        price: 5.99
+    },
+    {
+        bar_id: 1,
+        imagen: "https://amazingeventsapi.herokuapp.com/api/img/Cine7.jpg",
+        descripcion: "Hola",
+        Marca: "Apple",
+        Tipo: "Analogo, Band, Watch",
+        quantity: 10,
+        price: 5.99
+    },
+    {
+        bar_id: 1,
+        imagen: "https://amazingeventsapi.herokuapp.com/api/img/Cine7.jpg",
+        descripcion: "Hola",
+        Marca: "Apple",
+        Tipo: "Analogo, Band, Watch",
+        quantity: 10,
+        price: 5.99
+    },
+    {
+        bar_id: 1,
+        imagen: "https://amazingeventsapi.herokuapp.com/api/img/Cine7.jpg",
+        descripcion: "Hola",
+        Marca: "Apple",
+        Tipo: "Analogo, Band, Watch",
+        quantity: 10,
+        price: 5.99
+    },
+    {
+        bar_id: 1,
+        imagen: "https://amazingeventsapi.herokuapp.com/api/img/Cine7.jpg",
+        descripcion: "Hola",
+        Marca: "Apple",
+        Tipo: "Analogo, Band, Watch",
+        quantity: 10,
+        price: 5.99
+    },
+    {
+        bar_id: 1,
+        imagen: "https://amazingeventsapi.herokuapp.com/api/img/Cine7.jpg",
+        descripcion: "Hola",
+        Marca: "Apple",
+        Tipo: "Analogo, Band, Watch",
+        quantity: 10,
+        price: 5.99
+    },]
